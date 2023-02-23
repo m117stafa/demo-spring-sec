fff03abe3e9fc15f8662f986ebc3a0246a5283f2
m117stafa <mastaphaaz2001@gmail.com>
2023-02-23 18:55:32 +0100

    creating repositories

--- src/main/java/com/learning/demosec/repository/RoleRepository.java
+++ src/main/java/com/learning/demosec/repository/RoleRepository.java
@@ -1,7 +1,13 @@
 package com.learning.demosec.repository;
 
+import com.learning.demosec.models.ERole;
 import com.learning.demosec.models.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(ERole name);
 }
--- src/main/java/com/learning/demosec/repository/UserRepository.java
+++ src/main/java/com/learning/demosec/repository/UserRepository.java
@@ -2,6 +2,15 @@ package com.learning.demosec.repository;
 
 import com.learning.demosec.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    Boolean existByUsername(String username);
+
+    Boolean existByEmail(String email);
 }
