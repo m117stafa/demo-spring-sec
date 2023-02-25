4cf012996f1734095620bed58759308a81295f74
m117stafa <mastaphaaz2001@gmail.com>
2023-02-26 00:15:27 +0100

    adding jwt utilities

--- src/main/java/com/learning/demosec/security/jwt/JwtUtils.java
+++ src/main/java/com/learning/demosec/security/jwt/JwtUtils.java
@@ -21,17 +21,47 @@ public class JwtUtils {
 
     @Value("${demosec.app.jwtExpiration}")
     private int jwtExpiration;
+    byte[] jwtSecretByte = jwtSecret.getBytes();
 
     public String generateJwtToken(Authentication authentication){
         UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
+
+
 
         return Jwts.builder()
                 .setSubject((userPrincipal.getUsername()))
                 .setIssuedAt(new Date())
                 .setExpiration(new Date((new Date()).getTime() + jwtExpiration))
-                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .signWith(SignatureAlgorithm.HS512, jwtSecretByte)
                 .compact();
 
+    }
+
+    public String getUserNameFromJwtToken(String token){
+        return Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateJwtToken(String authToken) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+            return true;
+        } catch (SignatureException e) {
+            logger.error("Invalid JWT signature: {}", e.getMessage());
+        } catch (MalformedJwtException e) {
+            logger.error("Invalid JWT token: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            logger.error("JWT token is expired: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            logger.error("JWT token is unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            logger.error("JWT claims string is empty: {}", e.getMessage());
+        }
+
+        return false;
     }
 
 }
